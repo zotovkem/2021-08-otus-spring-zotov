0c91fd0e60a7972ba6f5c4345610500867c1c6a6
Evgeniy Zotov <e.zotov@carx-tech.com>
2021-09-14 17:11:47 +0700

    Правки по код ревью

--- 20210826-otus-spring-zotov-lab1/src/main/java/ru/otus/spring/utils/DataLoaderCsv.java
+++ 20210826-otus-spring-zotov-lab1/src/main/java/ru/otus/spring/utils/DataLoaderCsv.java
@@ -3,14 +3,11 @@ package ru.otus.spring.utils;
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.ClassPathResource;
+import ru.otus.spring.exception.CannotReadFileException;
 
-import java.io.File;
 import java.io.InputStream;
-import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * @author Created by ZotovES on 30.08.2021
@@ -27,8 +24,7 @@ public class DataLoaderCsv {
             MappingIterator<T> readValues = mapper.readerFor(type).with(bootstrapSchema).readValues(inputStream);
             return readValues.readAll();
         } catch (Exception e) {
-            System.err.println(e.getMessage());
-            return Collections.emptyList();
+            throw new CannotReadFileException("Error read file " + fileName);
         }
     }
 }
