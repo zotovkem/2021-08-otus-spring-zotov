3c022ece5bf8b5d5d2a85b9ea24805fe2d590676
Evgeniy Zotov <e.zotov@carx-tech.com>
2021-09-28 13:38:04 +0700

    Перевел приложение на spring boot

--- 20210921-otus-spring-zotov-hw3/src/main/java/ru/otus/spring/Application.java
+++ 20210921-otus-spring-zotov-hw3/src/main/java/ru/otus/spring/Application.java
@@ -2,28 +2,10 @@ package ru.otus.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import ru.otus.spring.config.PropertyService;
-import ru.otus.spring.service.InputService;
-import ru.otus.spring.service.QuestionService;
 
 @SpringBootApplication
 public class Application {
-
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-        QuestionService questionService = context.getBean(QuestionService.class);
-        PropertyService propertyService = context.getBean(PropertyService.class);
-        InputService inputService = context.getBean(InputService.class);
-
-        System.out.print("Please write you name: ");
-        String userName = inputService.getConsoleStrValue();
-
-        Integer countRightAnswerConsole = questionService.getCountRightAnswerConsole();
-
-        String resultTest = countRightAnswerConsole > propertyService.getCountRightAnswer() ? "passed" : "failed";
-
-        System.out.printf("Dear %s, count right answer %s. Test %s.%n", userName, countRightAnswerConsole, resultTest);
+        SpringApplication.run(Application.class, args);
     }
-
 }
