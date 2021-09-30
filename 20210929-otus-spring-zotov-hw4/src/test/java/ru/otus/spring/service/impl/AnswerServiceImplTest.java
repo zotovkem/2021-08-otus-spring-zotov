71cd531f42607fa6ffee5dc7f1844e7a47e68aa7
Evgeniy Zotov <e.zotov@carx-tech.com>
2021-09-30 13:06:43 +0700

    Тесты

--- 20210929-otus-spring-zotov-hw4/src/test/java/ru/otus/spring/service/impl/AnswerServiceImplTest.java
+++ 20210929-otus-spring-zotov-hw4/src/test/java/ru/otus/spring/service/impl/AnswerServiceImplTest.java
@@ -11,7 +11,9 @@ import ru.otus.spring.service.LocalizationService;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -38,5 +40,31 @@ class AnswerServiceImplTest {
 
         verify(answerDao).findByQuestionId(1);
         verify(localizationService).getLocalizationTextByTag(anyString(), anyList());
+    }
+
+    @Test
+    @DisplayName("Поиск ответа по номеру вопроса и варианту ответа")
+    void findByQuestionIdAndNumberTest() {
+        Optional<Answer> answer = Optional.of(new Answer(1, 1, 1, "test"));
+        when(answerDao.findByQuestionIdAndNumber(anyInt(), anyInt())).thenReturn(answer);
+
+        Optional<Answer> result = answerService.findByQuestionIdAndNumber(1, 1);
+
+        assertThat(result).isPresent().isEqualTo(answer);
+
+        verify(answerDao).findByQuestionIdAndNumber(1, 1);
+    }
+
+    @Test
+    @DisplayName("Поиск ответа по номеру вопроса и тексту ответа")
+    void findByQuestionIdAndNumberStringTest() {
+        Optional<Answer> answer = Optional.of(new Answer(1, 1, 1, "test"));
+        when(answerDao.findByQuestionIdAndNumber(anyInt(), anyString())).thenReturn(answer);
+
+        Optional<Answer> result = answerService.findByQuestionIdAndNumber(1, "if");
+
+        assertThat(result).isPresent().isEqualTo(answer);
+
+        verify(answerDao).findByQuestionIdAndNumber(1, "if");
     }
 }
--- 20210929-otus-spring-zotov-hw4/src/test/java/ru/otus/spring/service/impl/QuestionServiceImplTest.java
+++ 20210929-otus-spring-zotov-hw4/src/test/java/ru/otus/spring/service/impl/QuestionServiceImplTest.java
@@ -6,15 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import ru.otus.spring.dao.QuestionDao;
+import ru.otus.spring.model.Answer;
 import ru.otus.spring.model.Question;
 import ru.otus.spring.service.AnswerService;
 import ru.otus.spring.service.LocalizationService;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Matchers.anyInt;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 /**
  * @author Created by ZotovES on 30.08.2021
@@ -36,5 +39,59 @@ class QuestionServiceImplTest {
 
         verify(questionDao).findById(anyInt());
         verify(answerService).printAnswersByQuestionId(anyInt());
+    }
+
+    @Test
+    @DisplayName("Печать сообщения об окончании теста")
+    void printEndedTest() {
+        when(questionDao.findById(anyInt())).thenReturn(Optional.empty());
+
+        questionService.printQuestionById(1);
+
+        verify(questionDao).findById(anyInt());
+        verify(localizationService).getLocalizationTextByTag(anyString());
+        verify(answerService, never()).printAnswersByQuestionId(anyInt());
+    }
+
+    @Test
+    @DisplayName("Успешная проверка ответа по номеру варианта")
+    void checkAnswerByNumberTest() {
+        when(questionDao.findById(anyInt())).thenReturn(Optional.of(new Question(1, "testQuestion", "test")));
+        when(answerService.findByQuestionIdAndNumber(anyInt(), anyString())).thenReturn(Optional.of(new Answer(1, 1, 1, "test")));
+
+        boolean result = questionService.checkAnswer(1, "1");
+
+        assertTrue(result);
+
+        verify(questionDao).findById(anyInt());
+        verify(answerService).findByQuestionIdAndNumber(anyInt(), anyString());
+    }
+
+    @Test
+    @DisplayName("Не успешная проверка ответа по номеру варианта")
+    void notCheckAnswerByNumberTest() {
+        when(questionDao.findById(anyInt())).thenReturn(Optional.of(new Question(1, "testQuestion", "test")));
+        when(answerService.findByQuestionIdAndNumber(anyInt(), anyString())).thenReturn(Optional.empty());
+
+        boolean result = questionService.checkAnswer(1, "1");
+
+        assertFalse(result);
+
+        verify(questionDao).findById(anyInt());
+        verify(answerService).findByQuestionIdAndNumber(anyInt(), anyString());
+    }
+
+    @Test
+    @DisplayName("Успешная проверка ответа по тексту ответа")
+    void checkAnswerByTextTest() {
+        when(questionDao.findById(anyInt())).thenReturn(Optional.of(new Question(1, "testQuestion", "test")));
+        when(answerService.findByQuestionIdAndNumber(anyInt(), anyString())).thenReturn(Optional.of(new Answer(1, 1, 1, "test")));
+
+        boolean result = questionService.checkAnswer(1, "test");
+
+        assertTrue(result);
+
+        verify(questionDao).findById(anyInt());
+        verify(answerService, never()).findByQuestionIdAndNumber(anyInt(), anyString());
     }
 }
