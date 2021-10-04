89aa10112a1258701479faceedd8abb165b845b7
Evgeniy Zotov <e.zotov@carx-tech.com>
2021-10-04 12:08:34 +0700

    Скрипты БД

--- 20210921-otus-spring-zotov-hw5/src/main/java/ru/zotov/hw5/Hw5Application.java
+++ 20210921-otus-spring-zotov-hw5/src/main/java/ru/zotov/hw5/Hw5Application.java
@@ -1,12 +1,16 @@
 package ru.zotov.hw5;
 
+import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class Hw5Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Console.main(args);
         SpringApplication.run(Hw5Application.class, args);
     }
 
