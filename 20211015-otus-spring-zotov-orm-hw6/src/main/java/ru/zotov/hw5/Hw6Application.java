2fda6c7ae5d79f8a7873db9bad239d1fe7b37b14
Evgeniy Zotov <e.zotov@carx-tech.com>
2021-10-21 20:52:33 +0700

    Сущности

--- 20211015-otus-spring-zotov-orm-hw6/src/main/java/ru/zotov/hw5/Hw6Application.java
+++ 20211015-otus-spring-zotov-orm-hw6/src/main/java/ru/zotov/hw5/Hw6Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Hw5Application {
+public class Hw6Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Hw5Application.class, args);
+        SpringApplication.run(Hw6Application.class, args);
     }
 
 }
--- 20211015-otus-spring-zotov-orm-hw6/src/main/java/ru/zotov/hw5/domain/Book.java
+++ 20211015-otus-spring-zotov-orm-hw6/src/main/java/ru/zotov/hw5/domain/Book.java
@@ -1,50 +1,85 @@
 package ru.zotov.hw5.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Objects;
 
 /**
  * @author Created by ZotovES on 29.09.2021
  * Книга
  */
-@Data
-@Builder
+@Getter
+@Setter
+@Entity
+@ToString
+@NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "book")
 public class Book {
     /**
      * Ид книги
      */
+    @Id
+    @Column(name = "id")
     private Long id;
     /**
      * Наименование книги
      */
+    @Column(name = "name")
     private String name;
     /**
      * Год издания
      */
+    @Column(name = "release_year")
     private int releaseYear;
     /**
      * Авторы
      */
     @Builder.Default
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "mtm_book_author", joinColumns = {@JoinColumn(name = "book_id")},
+               inverseJoinColumns = {@JoinColumn(name = "author_id")})
     private List<Author> authors = new ArrayList<>();
     /**
      * Жанры
      */
     @Builder.Default
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "mtm_book_genres", joinColumns = {@JoinColumn(name = "book_id")},
+               inverseJoinColumns = {@JoinColumn(name = "genre_id")})
     private List<Genre> genres = new ArrayList<>();
 
-    public String toString() {
-        return String.format("Ид: %s%nНаименование книги: %s%nГод издательства: %s%n" +
-                        "Авторы: %s%n" +
-                        "Жанры: %s%n" +
-                        "=====================================", getId(), getName(), getReleaseYear(),
-                getAuthors().stream().map(Author::getFio).collect(Collectors.joining(", ")),
-                getGenres().stream().map(Genre::getName).collect(Collectors.joining(", ")));
+//TODO
+//    public String toString() {
+//        return String.format("Ид: %s%nНаименование книги: %s%nГод издательства: %s%n" +
+//                        "Авторы: %s%n" +
+//                        "Жанры: %s%n" +
+//                        "=====================================", getId(), getName(), getReleaseYear(),
+//                getAuthors().stream().map(Author::getFio).collect(Collectors.joining(", ")),
+//                getGenres().stream().map(Genre::getName).collect(Collectors.joining(", ")));
+//    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Book book = (Book) o;
+        return releaseYear == book.releaseYear && Objects.equals(id, book.id) && Objects.equals(name, book.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, releaseYear);
     }
 }
