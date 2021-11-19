7b6c858568086afa43822614f3728661c80f95ed
Evgeniy Zotov <e.zotov@carx-tech.com>
2021-11-19 18:36:04 +0700

    Интерфейс жанров

--- 20211115-otus-spring-zotov-spring-mvc-hw10/frontend-js/book-app/src/App.js
+++ 20211115-otus-spring-zotov-spring-mvc-hw10/frontend-js/book-app/src/App.js
@@ -4,9 +4,15 @@ import {dataProvider} from './provider/DataProvider';
 import polyglotI18nProvider from 'ra-i18n-polyglot';
 import russianMessages from 'ra-language-russian';
 import UserIcon from '@material-ui/icons/Group';
+import LibraryBooksIcon from '@material-ui/icons/LibraryBooks';
+import MenuBook from '@material-ui/icons/MenuBook';
+import Comment from '@material-ui/icons/Comment';
 import {authorEdit} from "./component/author/AuthorEditForm";
 import {authorCreate} from "./component/author/AuthorCreateForm";
 import {AuthorList} from "./component/author/AuthorList";
+import {GenreList} from "./component/genre/GenreList";
+import {genreEdit} from "./component/genre/GenreEditForm";
+import {genreCreate} from "./component/genre/GenreCreateForm";
 
 const i18nProvider = polyglotI18nProvider(() => russianMessages, 'ru');
 
@@ -14,7 +20,10 @@ function App() {
     return (
         <div className="App">
             <Admin title="Library" dataProvider={dataProvider} i18nProvider={i18nProvider}>
+                <Resource name={"books"} list={GenreList} edit={genreEdit}  create={genreCreate} icon={MenuBook} options={{ label: 'Книги' }} />
+                <Resource name={"comments"} list={GenreList} edit={genreEdit}  create={genreCreate} icon={Comment} options={{ label: 'Комментарии' }} />
                 <Resource name={"authors"} list={AuthorList} edit={authorEdit} create={authorCreate} icon={UserIcon} options={{ label: 'Авторы' }} />
+                <Resource name={"genres"} list={GenreList} edit={genreEdit}  create={genreCreate} icon={LibraryBooksIcon} options={{ label: 'Жанры' }} />
             </Admin>;
         </div>
     )
