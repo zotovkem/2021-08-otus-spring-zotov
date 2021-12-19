7f9fed85b134304cf4857dafb91f94cfa6e53086
Evgeniy Zotov <e.zotov@carx-tech.com>
2021-12-19 12:05:27 +0700

    ACL права

--- 20211203-otus-spring-zotov-auth-hw13/src/main/resources/data.sql
+++ 20211203-otus-spring-zotov-auth-hw13/src/main/resources/data.sql
@@ -35,8 +35,8 @@ VALUES (1, 1, 'Вроде не чего, еще не дочитал', 'Зото�
        (7, 1, 'Комментарий ', 'Иванов', '2020-02-01 19:10:25-07'),
        (8, 2, 'Тестовый комментарий', 'Петров', '2020-02-01 19:10:25-07'),
        (9, 3, 'Еще один комментарий', 'Сидоров', '2020-02-01 19:10:25-07'),
-       (10, 4, 'Как много комментарием нужно написать', 'Тестов', '2020-02-01 19:10:25-07'),
-       (12, 6, 'Последний комментарий', 'Лютый критик', '2020-02-01 19:10:25-07');
+       (10, 5, 'Как много комментарием нужно написать', 'Тестов', '2020-02-01 19:10:25-07'),
+       (11, 6, 'Последний комментарий', 'Лютый критик', '2020-02-01 19:10:25-07');
 
 
 INSERT INTO mtm_book_author (id, book_id, author_id)
@@ -70,16 +70,32 @@ VALUES (1, 0, 'ROLE_ADMIN'),
        (3, 0, 'ROLE_CHILD');
 
 INSERT INTO acl_class (id, class)
-VALUES (1, 'ru.zotov.hw13.domain.Book');
+VALUES (1, 'ru.zotov.hw13.domain.Book'),
+       (2, 'ru.zotov.hw13.domain.Comment');
 
 INSERT INTO acl_object_identity (id, object_id_class, object_id_identity, parent_object, owner_sid, entries_inheriting)
-VALUES (1, 1, 1, NULL, 1, 0),
-       (2, 1, 2, NULL, 1, 0),
-       (3, 1, 3, NULL, 1, 0),
-       (4, 1, 4, NULL, 1, 0),
-       (5, 1, 5, NULL, 1, 0),
-       (6, 1, 6, NULL, 1, 0);
+VALUES
+    -- Книги
+    (1, 1, 1, NULL, 1, 0),
+    (2, 1, 2, NULL, 1, 0),
+    (3, 1, 3, NULL, 1, 0),
+    (4, 1, 4, NULL, 1, 0),
+    (5, 1, 5, NULL, 1, 0),
+    (6, 1, 6, NULL, 1, 0),
+    -- Комментарии
+    (7, 2, 1, NULL, 1, 0),
+    (8, 2, 2, NULL, 1, 0),
+    (9, 2, 3, NULL, 1, 0),
+    (10, 2, 4, NULL, 1, 0),
+    (11, 2, 5, NULL, 1, 0),
+    (12, 2, 6, NULL, 1, 0),
+    (13, 2, 7, NULL, 1, 0),
+    (14, 2, 8, NULL, 1, 0),
+    (15, 2, 9, NULL, 1, 0),
+    (16, 2, 10, NULL, 1, 0),
+    (17, 2, 11, NULL, 1, 0);
 
+-- Книги
 INSERT INTO acl_entry (id, acl_object_identity, ace_order, sid, mask, granting, audit_success, audit_failure)
 VALUES
     --Права админа
@@ -103,3 +119,13 @@ VALUES
     (17, 4, 3, 3, 1, 0, 1, 1),
     (18, 5, 3, 3, 1, 1, 1, 1),
     (19, 6, 3, 3, 1, 0, 1, 1);
+-- Комментарии
+INSERT INTO acl_entry (id, acl_object_identity, ace_order, sid, mask, granting, audit_success, audit_failure)
+VALUES
+    --Права ребенка
+    (54, 10, 3, 3, 1, 1, 1, 1);
+/*public static final Permission READ = new BasePermission(1 << 0, 'R'); // 1
+	public static final Permission WRITE = new BasePermission(1 << 1, 'W'); // 2
+	public static final Permission CREATE = new BasePermission(1 << 2, 'C'); // 4
+	public static final Permission DELETE = new BasePermission(1 << 3, 'D'); // 8
+	public static final Permission ADMINISTRATION = new BasePermission(1 << 4, 'A'); // 16*/
