23c61c69fe2ee16d2514b7af8cb71146dac74b42
Evgeniy Zotov <e.zotov@carx-tech.com>
2022-01-05 23:41:13 +0700

    Процессоры справочников

--- 20211227-otus-spring-zotov-spring-batch-hw14/src/main/java/ru/zotov/hw14/utils/BatchJobContextHelper.java
+++ 20211227-otus-spring-zotov-spring-batch-hw14/src/main/java/ru/zotov/hw14/utils/BatchJobContextHelper.java
@@ -1,13 +1,11 @@
 package ru.zotov.hw14.utils;
 
 import org.springframework.batch.core.StepExecution;
-import org.springframework.batch.core.scope.context.StepContext;
+import org.springframework.batch.item.ExecutionContext;
 import ru.zotov.hw14.constant.MigrationTableName;
 
 import java.util.HashMap;
 import java.util.Map;
-
-import static ru.zotov.hw14.constant.MigrationTableName.AUTHOR;
 
 /**
  * @author Created by ZotovES on 05.01.2022
@@ -16,16 +14,34 @@ import static ru.zotov.hw14.constant.MigrationTableName.AUTHOR;
 public class BatchJobContextHelper {
     /**
      * Получить мапу по имени таблицы из контекста джобы
-     * @param stepExecution контекст шага
+     *
+     * @param stepExecution      контекст шага
      * @param migrationTableName имя таблицы
      * @return мапа внешнего ид и ид энтити
      */
     @SuppressWarnings("unchecked")
-    public static Map<String, Long> getContextMapByTableNameEnum(StepExecution stepExecution, MigrationTableName migrationTableName) {
+    public static Map<String, Long> getContextMapByTableNameEnum(StepExecution stepExecution,
+            MigrationTableName migrationTableName) {
         Object map = stepExecution.getJobExecution().getExecutionContext().get(migrationTableName.name());
         if (map instanceof Map) {
             return ((Map<String, Long>) map);
         }
-        return new HashMap<String, Long>();
+        return new HashMap<>();
+    }
+
+    /**
+     * Сохраняет в контекст шага информацию об импортированных записях
+     *
+     * @param addedRow      сохраненные записи
+     * @param stepExecution контекст шага
+     * @param tableNameEnum энум имени таблицы
+     */
+    public static void saveStepExecutionContext(Map<String, Long> addedRow, StepExecution stepExecution,
+            MigrationTableName tableNameEnum) {
+        ExecutionContext stepExecutionContext = stepExecution.getExecutionContext();
+        Map<String, Long> contextMap = getContextMapByTableNameEnum(stepExecution, tableNameEnum);
+        contextMap.putAll(addedRow);
+
+        stepExecutionContext.put(tableNameEnum.name(), contextMap);
     }
 }
