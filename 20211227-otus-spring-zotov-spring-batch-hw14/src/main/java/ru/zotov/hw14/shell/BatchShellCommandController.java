b709982af991be50f78ab70ee5bc3d6e39363bf9
Evgeniy Zotov <e.zotov@carx-tech.com>
2022-01-05 22:56:44 +0700

    Контроллер команд

--- 20211227-otus-spring-zotov-spring-batch-hw14/src/main/java/ru/zotov/hw14/shell/BatchShellCommandController.java
+++ 20211227-otus-spring-zotov-spring-batch-hw14/src/main/java/ru/zotov/hw14/shell/BatchShellCommandController.java
@@ -1,27 +1,31 @@
 package ru.zotov.hw14.shell;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
-import org.springframework.shell.standard.ShellOption;
-
-import java.util.List;
 
 /**
- * @author Created by ZotovES on 08.10.2021
+ * @author Created by ZotovES on 05.01.2022
  * Контроллер команд управления миграцией
  */
 @SuppressWarnings("unused")
 @ShellComponent
 @RequiredArgsConstructor
 public class BatchShellCommandController {
+    private final Job migrationBookJob;
+    private final JobLauncher jobLauncher;
+    private final JobExplorer jobExplorer;
 
     /**
-     * Рестарт задания миграции
-     *
+     * Старт задания миграции
      */
-    @ShellMethod(value = "Restart migration", key = {"restart"})
-    public void restartMigration() {
-
+    @ShellMethod(value = "startMigration", key = "start")
+    public void startMigrationJobWithJobLauncher() throws Exception {
+        jobLauncher.run(migrationBookJob,
+                new JobParametersBuilder(jobExplorer).getNextJobParameters(migrationBookJob).toJobParameters());
     }
 }
