e2fff633df34d64a10dba9c93f2cead07ca7e910
Evgeniy Zotov <e.zotov@carx-tech.com>
2022-01-06 20:09:50 +0700

    Зависимости,схема, проперти

--- 20211227-otus-spring-zotov-spring-batch-hw14/src/main/resources/schema.sql
+++ 20211227-otus-spring-zotov-spring-batch-hw14/src/main/resources/schema.sql
@@ -76,80 +76,17 @@ COMMENT ON COLUMN comment_for_book.book_id IS 'Ид книги';
 COMMENT ON COLUMN comment_for_book.content IS 'Текст комментария';
 COMMENT ON COLUMN comment_for_book.author IS 'Автор комментария';
 COMMENT ON COLUMN comment_for_book.create_date IS 'Дата комментария';
-///Security
-DROP TABLE IF EXISTS user_library;
-CREATE TABLE user_library
+
+DROP TABLE IF EXISTS migration_registry;
+CREATE TABLE migration_registry
 (
-    id       IDENTITY PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    role     VARCHAR(255) NOT NULL
+    id          IDENTITY PRIMARY KEY,
+    entity_id     BIGINT                   NOT NULL,
+    external_id     VARCHAR(255),
+    table_name      VARCHAR(255)
 );
-COMMENT ON TABLE user_library IS 'Пользователи';
-COMMENT ON COLUMN user_library.id IS 'Ид';
-COMMENT ON COLUMN user_library.username IS 'Имя пользователя';
-COMMENT ON COLUMN user_library.password IS 'Пароль';
-
-///ACL
-DROP TABLE IF EXISTS acl_sid;
-CREATE TABLE IF NOT EXISTS acl_sid
-(
-    id        bigint(20)   NOT NULL AUTO_INCREMENT,
-    principal tinyint(1)   NOT NULL,
-    sid       varchar(100) NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE KEY unique_uk_1 (sid,principal)
-);
-
-CREATE TABLE IF NOT EXISTS acl_class
-(
-    id    bigint(20)   NOT NULL AUTO_INCREMENT,
-    class varchar(255) NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE KEY unique_uk_2 (class)
-);
-
-CREATE TABLE IF NOT EXISTS acl_entry
-(
-    id                  bigint(20) NOT NULL AUTO_INCREMENT,
-    acl_object_identity bigint(20) NOT NULL,
-    ace_order           int(11)    NOT NULL,
-    sid                 bigint(20) NOT NULL,
-    mask                int(11)    NOT NULL,
-    granting            tinyint(1) NOT NULL,
-    audit_success       tinyint(1) NOT NULL,
-    audit_failure       tinyint(1) NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE KEY unique_uk_4 (acl_object_identity,ace_order)
-);
-
-CREATE TABLE IF NOT EXISTS acl_object_identity
-(
-    id                 bigint(20) NOT NULL AUTO_INCREMENT,
-    object_id_class    bigint(20) NOT NULL,
-    object_id_identity bigint(20) NOT NULL,
-    parent_object      bigint(20) DEFAULT NULL,
-    owner_sid          bigint(20) DEFAULT NULL,
-    entries_inheriting tinyint(1) NOT NULL,
-    PRIMARY KEY (id),
-    UNIQUE KEY unique_uk_3 (object_id_class,object_id_identity)
-);
-commit;
-
-ALTER TABLE acl_entry
-    ADD FOREIGN KEY (acl_object_identity) REFERENCES acl_object_identity (id);
-
-ALTER TABLE acl_entry
-    ADD FOREIGN KEY (sid) REFERENCES acl_sid (id);
-
---
--- Constraints for table acl_object_identity
---
-ALTER TABLE acl_object_identity
-    ADD FOREIGN KEY (parent_object) REFERENCES acl_object_identity (id);
-
-ALTER TABLE acl_object_identity
-    ADD FOREIGN KEY (object_id_class) REFERENCES acl_class (id);
-
-ALTER TABLE acl_object_identity
-    ADD FOREIGN KEY (owner_sid) REFERENCES acl_sid (id);
+COMMENT ON TABLE migration_registry IS 'Реестр перенесенных записей';
+COMMENT ON COLUMN migration_registry.id IS 'Ид';
+COMMENT ON COLUMN migration_registry.entity_id IS 'Ид записи в целевой БД';
+COMMENT ON COLUMN migration_registry.external_id IS 'Ид во внешней БД';
+COMMENT ON COLUMN migration_registry.table_name IS 'Имя таблицы';
